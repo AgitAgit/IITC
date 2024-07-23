3bfd6e898a758190ed64c761c5f4b076b17fc5e6
AgitAgit <amitkubani@gmail.com>
2024-07-23 15:26:18 +0300

    A commit

--- 2024-07/2024-07-23/main.js
+++ 2024-07/2024-07-23/main.js
@@ -1,1 +1,11 @@
-console.log(5**2);
+
+function addStrings(string1, string2){
+    let result = string1 + string2;
+    return result;
+}
+
+let someName = addStrings("Ron", "Sherling");
+
+let otherName = addStrings("Amit","Kubani");
+
+console.log(someName);
