85844d43b20e38abb09b9330a6b712cbd80ebd9e
Amit Kubani <amitkubani@gmail.com>
2024-07-26 11:43:20 +0300

    commit

---
+++ Homework/week1/task1/main.js
@@ -0,0 +1,42 @@
+let firstName = "Amit";
+let age = 25;
+let isRaining = false;
+const PI = 3.14159;
+let isStudent = true;
+let favoriteColor = "";
+//let temperature = 72.5;
+const DAYS_IN_WEEK = 7;
+let username = 'coder123';
+let score = 0;
+let isLoggedIn = false;
+const COMPANY_NAME = 'Tech Solutions Inc.';
+let currentYear = new Date().getFullYear();
+let price = 19.99;
+let quantity = 100;
+let counter = 0;
+counter++;
+let message = "Hello";
+message = 'Hello, World!';
+let myVariable = 42;
+myVariable = 'forty-two';
+let totalCost = 50;
+totalCost += 10;
+username = "user1";
+username = 'admin';
+let temperature = 20;
+temperature = (temperature * 1.8) + 32;
+let fruits = ['apple','banana'];
+fruits.push('orange');
+person = {
+    name:"",
+    age:""
+}
+person.city = "";
+console.dir(person);
+
+let count = 5;
+count--;
+
+let greeting = 'Good';
+greeting += ' morning';
+console.log(greeting);
