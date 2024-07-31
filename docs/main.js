3839df946ae16ed88dcd84a2e3ebbd5117de43e0
agit <box999box@gmail.com>
2024-07-31 21:43:03 +0300

    commit

--- docs/main.js
+++ docs/main.js
@@ -180,8 +180,11 @@ function abandonedBarn(){
 }
 
 function start(){
-    //player = prompt("Enter your name");
-    //gunSound = prompt("Enter the sound a gun should make");
+    if(beenTo[0] === "")
+    {
+        player = prompt("Enter your name");
+        gunSound = prompt("Enter the sound a gun should make");
+    }
     health = 100;
     moves = -1;
     score = 0;
