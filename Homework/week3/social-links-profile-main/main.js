3960f62f5818932297c220774ad8ac0424961597
Amit Kubani <amitkubani@gmail.com>
2024-08-04 14:33:14 +0300

    commit

--- Homework/week3/social-links-profile-main/main.js
+++ Homework/week3/social-links-profile-main/main.js
@@ -5,12 +5,16 @@ function btn(name, link){
     this.link = link;
 }
 
-let buttons = [new btn("Github","www.example.com"),new btn("linkedin","www.example.com")];
+let buttons = [new btn("Github","https://github.com/AgitAgit/IITC/tree/8f58c3d839b17497b3de4bf9dd868171e90b88b0/Homework/week3/social-links-profile-main"),
+                new btn("Linkedin","https://youtu.be/dQw4w9WgXcQ?si=3Vxhg2d3jed9dr27")];
 
 function init(){
     for(const button of buttons){
         let btn = document.createElement('button');
         btn.textContent = button.name;
+        btn.addEventListener('click', () =>{
+            window.open(button.link, '_blank');
+        });
         mainDiv.appendChild(btn);
     }
 }
