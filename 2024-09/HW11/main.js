5f1fd84ce59cf3fb848c22fa6662159e3c3bd06d
agit <box999box@gmail.com>
2024-09-11 22:16:21 +0300

    commit

---
+++ 2024-09/HW11/main.js
@@ -0,0 +1,29 @@
+const num1 = document.getElementById("num1");//10
+let value1 = 0;
+const num2 = document.getElementById("num2");//314
+let value2 = 0;
+const num3 = document.getElementById("num3");//12
+let value3 = 0;
+
+function incrementNum1(inc){
+    if(value1 >= 11) return;
+    num1.textContent = value1;
+    value1++;
+    setTimeout(() => incrementNum1(inc + 30), inc);
+}
+function incrementNum2(inc){
+    if(value2 >= 315) return;
+    num2.textContent = value2;
+    value2+= 5;
+    setTimeout(() => incrementNum2(inc + 1.5), inc);
+}
+function incrementNum3(inc){
+    if(value3 >= 13) return;
+    num3.textContent = value3;
+    value3++;
+    setTimeout(() => incrementNum3(inc + 30), inc);
+}
+
+incrementNum1(30);
+incrementNum2(0);
+incrementNum3(30);
