8b03f99476d4805030b37be9c4e370c5c8d80522
Amit Kubani <amitkubani@gmail.com>
2024-09-12 11:50:47 +0300

    commit

--- 2024-09/HW11/main.js
+++ 2024-09/HW11/main.js
@@ -6,24 +6,33 @@ const num3 = document.getElementById("num3");//12
 let value3 = 0;
 
 function incrementNum1(inc){
-    if(value1 >= 11) return;
-    num1.textContent = value1;
+    if(value1 >= 11) {
+        num1.style.color = 'hsl(277, 64%, 61%)';
+        return;
+    }
+    num1.textContent = value1 + 'K+';
     value1++;
     setTimeout(() => incrementNum1(inc + 30), inc);
 }
 function incrementNum2(inc){
-    if(value2 >= 315) return;
+    if(value2 >= 315) {
+        num2.style.color = 'hsl(277, 64%, 61%)';
+        return;
+    }
     num2.textContent = value2;
     value2+= 5;
-    setTimeout(() => incrementNum2(inc + 1.5), inc);
+    setTimeout(() => incrementNum2(inc + 1.2), inc);
 }
 function incrementNum3(inc){
-    if(value3 >= 13) return;
-    num3.textContent = value3;
+    if(value3 >= 13) {
+        num3.style.color = 'hsl(277, 64%, 61%)';
+        return;
+    }
+    num3.textContent = value3 + 'M+';
     value3++;
     setTimeout(() => incrementNum3(inc + 30), inc);
 }
 
 incrementNum1(30);
 incrementNum2(0);
-incrementNum3(30);
+incrementNum3(90);
