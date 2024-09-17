f40216c6a2ae8a997ff7b5adf4b6497d325c2cd0
Amit Kubani <amitkubani@gmail.com>
2024-09-17 15:19:06 +0300

    commit

--- 2024-09/2024-09-17/main.js
+++ 2024-09/2024-09-17/main.js
@@ -1,12 +1,42 @@
+//task1
+const _task1 = document.getElementById('task1');
 const _Content = document.getElementById('content');
 const _Button = document.getElementsByTagName('button')[0];
+const items = document.getElementById('list').querySelectorAll('li');
 _Content.classList.add('blue');
+_Button.addEventListener('click', handleButtonClick)
+
+//task2
+const _task2 = document.getElementById('task2');
+const _list2 = document.getElementById('list2');
+const _button2 = document.getElementById('button2');
+const _btnRemoveFirst = document.getElementById('removeFirst');
+const _btnRemoveLast = document.getElementById('removeLast');
+const _btnRemoveSelected = document.getElementById('removeSelected');
+let counter = 0;
+_button2.addEventListener('click', addItem);
+_btnRemoveFirst.addEventListener('click', removeFirstItem);
+_btnRemoveLast.addEventListener('click',removeLastItem);
+_btnRemoveSelected.addEventListener('click', removeSelectedItem);
+
+//task3
+const _greeting = document.getElementById('greeting');
+const _input = document.getElementById('nameInput');
+const _clear = document.getElementById('clear');
+_input.addEventListener('input', handleInputChange);
+_clear.addEventListener('click', handleClearClick);
+
+//task4
+const _box = document.getElementById('box');
+const _btnStyle = document.getElementById('changeStyle');
+_btnStyle.addEventListener('click', changeBox);
+
+
 
 document.querySelectorAll('.text').forEach(element => {
     element.style = "color:red;";
 });
 
-const items = document.getElementById('list').querySelectorAll('li');
 
 for(let i = 0; i < items.length; i++){
     console.log(items[i].textContent);
@@ -18,4 +48,61 @@ function handleButtonClick(){
     }
 }
 
-_Button.addEventListener('click', handleButtonClick)
+function toggleSelect(event){
+
+    if(event.target.classList.contains('selectedItem')){
+        event.target.classList.remove('selectedItem')
+    } 
+    else {
+        event.target.classList.add('selectedItem');
+        for(let i = 0; i < _list2.children.length; i++){
+            const child = _list2.children[i];
+            if(child !== event.target){
+                child.classList.remove('selectedItem');
+            }
+        }
+    }
+}
+
+function addItem(){
+    const li = document.createElement('li');
+    counter++;    
+    li.textContent = `list item #${counter}`;
+    li.addEventListener('click', toggleSelect);
+    _list2.appendChild(li);
+}
+
+function removeFirstItem(){
+    _list2.firstChild.remove();
+}
+function removeLastItem(){
+    _list2.lastChild.remove();
+}
+function removeSelectedItem(){
+    for(let i = 0; i < _list2.children.length; i++){
+        const child = _list2.children[i];
+        if(child.classList.contains('selectedItem')){
+            child.remove();
+        }
+    }
+}
+addItem();
+addItem();
+addItem();
+
+
+function handleInputChange(){
+    _greeting.textContent = `Hello, ${_input.value}!`;
+}
+
+function handleClearClick(){
+    _input.value = '';
+    handleInputChange();
+}
+
+function changeBox(){
+    const red = Math.random() * 255;
+    const green = Math.random() * 255;
+    const blue = Math.random() * 255;
+    _box.style.borderColor = `rgb(${red},${green},${blue})`;
+}
