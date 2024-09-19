8883ec5793f87ed5f4401b6779adbb72fc7194c2
Amit Kubani <amitkubani@gmail.com>
2024-09-19 13:25:24 +0300

    commit

--- 2024-09/2024-09-18/main.js
+++ 2024-09/2024-09-18/main.js
@@ -1,7 +1,8 @@
 const input1 = document.getElementById('input1');
 const add1 = document.getElementById('add1');
 const list1 = document.getElementById('list1');
-//let counter = parseInt(localStorage.getItem('counter')) + 1 || 0;
+const items = JSON.parse(localStorage.getItem('items')) || [];
+let counter = parseInt(localStorage.getItem('counter')) || 0;
 add1.addEventListener('click', addItem);
 
 function addItem(){
@@ -21,6 +22,11 @@ function addItem(){
 
     if(li.textContent !== ''){
         list1.appendChild(li);
+        counter++;
+        localStorage.setItem('counter',counter);
+        items.push({'id':counter, 'item':li.textContent, 'done':false});
+        localStorage.setItem(`items`, JSON.stringify(items));
+        console.log(items);
     }
 }
 
@@ -32,5 +38,30 @@ input1.value = 'item3';
 addItem();
 
 function onStart(){
+    items.forEach((item)=>{
+        //list1.appendChild(item.item);
+    })
+}
+onStart();
 
+function createItem(text, done){
+    const li = document.createElement('li');
+    li.addEventListener('click',()=>{
+        li.classList.toggle('done');
+    });
+    li.textContent = text;
+    (done === true ? li.classList.add('done'):li.classList.remove('done'));
+    
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = '🗑️';
+    deleteBtn.addEventListener('click', function(event){
+        event.target.parentElement.remove();
+    });
+    li.appendChild(deleteBtn);
+
+    return li;
+}
+
+function addItem(li){
+    list1.appendChild(li);
 }
