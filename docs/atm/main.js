e25de4ccf2fe29124047ded51083cd6fceccd365
Amit Kubani <amitkubani@gmail.com>
2024-10-06 16:17:35 +0300

    commit

--- docs/atm/main.js
+++ docs/atm/main.js
@@ -25,10 +25,17 @@ const _displayDiv = document.getElementById('displayDiv');
 const _loginForm = document.getElementById('loginDiv');
 const _menuDiv = document.getElementById('menuDiv');
 const _displayBalanceDiv = document.getElementById('displayBalanceDiv');
+const _displayBalanceSpan = document.getElementById('balanceSpan');
 const _depositDiv = document.getElementById('depositDiv');
 const _withdrawDiv = document.getElementById('withdrawDiv');
+const _returnButtons = document.querySelectorAll('.returnBtn');
 
-_loginForm.addEventListener('submit', handleSubmitForm);
+_returnButtons.forEach(button => {
+    button.addEventListener('click', (event) => goToMenu(event));
+});
+_loginForm.addEventListener('submit', handleSubmitLogin);
+_depositDiv.addEventListener('submit', handleDepositSubmit);
+_withdrawDiv.addEventListener('submit', handleWithdrawSubmit);
 
 const _displayElements = [_loginForm, _menuDiv, _displayBalanceDiv, _depositDiv, _withdrawDiv];
 
@@ -41,11 +48,13 @@ function goto(from, to){
     from.classList.add('hidden');
     to.classList.remove('hidden');
 }
-function goToMenu(){
+function goToMenu(e){
+    e.preventDefault();
     goTo(_menuDiv);
 }
 function goToDisplayBalance(){
     goTo(_displayBalanceDiv);
+    displayBalance();
 }
 function goToDeposit(){
     goTo(_depositDiv);
@@ -58,19 +67,10 @@ function goToLogin(){
     goTo(_loginForm);
 }
 
-//verify user
-function handleEnterPIN(){
-    if(verifyPIN()){
-        goToMenu();
-    }
-}
-function verifyPIN(pin='1234'){
-    return true;
-}
 
 //get user data
 let currentUser = null;
-function handleSubmitForm(event){
+function handleSubmitLogin(event){
     event.preventDefault();
     const formData = new FormData(_loginForm);
     const data = Object.fromEntries(formData);
@@ -84,12 +84,50 @@ function handleSubmitForm(event){
     console.log("current user:", currentUser);
     console.log("users:",users);
     commitToLocalStorage();
+    goToMenu(event);
 }
 function getUserById(pin){
     return users.find(user => user.PIN === pin);
 }
 
-//manipulate user data
+//manipulate and display user data
+function displayBalance(){
+    _displayBalanceSpan.textContent = currentUser.Balance;
+}
+
+function deposit(sum){
+    currentUser.Balance += sum;
+}
+function handleDepositSubmit(event){
+    event.preventDefault();
+    const formData = new FormData(_depositDiv);
+    const data = Object.fromEntries(formData);
+    const sum = parseInt(data.depositSum);
+    deposit(sum);
+    commitToLocalStorage();
+}
+
+function withdraw(sum){
+    if(sum <= currentUser.Balance){
+        currentUser.Balance -= sum;
+        return true;
+    }
+    return false;
+}
+function handleWithdrawSubmit(event){
+    event.preventDefault();
+    const formData = new FormData(_withdrawDiv);
+    const data = Object.fromEntries(formData);
+    console.log('withdraw data',data);
+    const sum = parseInt(data.withdrawSum);
+    if(withdraw(sum)){
+        console.log('withdraw successful. Your new balance is:', currentUser.Balance);
+    }
+    else{
+        console.log('Can not withdraw this sum. Your current balance is:', currentUser.Balance);
+    }
+    commitToLocalStorage();
+}
 
 
 
