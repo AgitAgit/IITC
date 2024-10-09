881163f3430b304f24c7209c96943ecf3f2c0ac4
Amit Kubani <amitkubani@gmail.com>
2024-10-09 15:03:26 +0300

    commit

--- 2024-10/interactive-rating-component-main/main.js
+++ 2024-10/interactive-rating-component-main/main.js
@@ -3,22 +3,38 @@ const _rating = document.querySelector('.ratingDiv');
 const _submit = document.getElementById('submitBtn');
 
 const _thankYou = document.querySelector('.thankYou');
+const _ratingSpan = document.getElementById('ratingSpan');
 
 _submit.addEventListener('click', handleSubmit);
 
-let rating;
+let rating = 3;
 
 function initRating(){
-
+    for(let i = 1; i < 6; i++){
+        const button = document.createElement('button');
+        button.classList.add('roundButton');
+        button.innerText = i;
+        button.addEventListener('click', (event) => {
+            event.preventDefault();
+            const buttons = document.querySelectorAll('.roundButton');
+            buttons.forEach(btn => btn.classList.remove('clicked'));
+            event.target.classList.add('clicked');
+            rating = i;
+        });
+        _rating.appendChild(button);
+    }
 }
 
 function handleSubmit(event){
     event.preventDefault();
     _form.classList.toggle('hidden');
     _thankYou.classList.toggle('hidden');
+    _ratingSpan.textContent = rating;
 }
 
 function toggle(){
     _form.classList.toggle('hidden');
     _thankYou.classList.toggle('hidden');
 }
+
+initRating();
