d7113f18a50ede1fef42cec1279745a6314812a7
Amit Kubani <amitkubani@gmail.com>
2024-10-10 16:07:40 +0300

    commit

--- 2024-10/basic_api_calls/main.js
+++ 2024-10/basic_api_calls/main.js
@@ -1,6 +1,8 @@
 const _alterImage = document.getElementById('alternatingImg');
+const _gif = document.querySelector('.tenor-gif-embed');
 
 function handleApiClick(event=null, random=null){
+    _gif.classList.add('hidden');
     let api;
     if(random){
         api = random;
@@ -13,6 +15,7 @@ function handleApiClick(event=null, random=null){
         _alterImage.src = "https://cataas.com/cat";
     }
     if(api === 'api1'){
+        _alterImage.classList.toggle('hidden');
         fetch('https://cataas.com/cat')
         .then(response => {
             return response.blob();
@@ -21,10 +24,12 @@ function handleApiClick(event=null, random=null){
             const imgUrl = URL.createObjectURL(blob);
             _alterImage.src = imgUrl;
             //URL.revokeObjectURL(imgUrl);
+            _alterImage.classList.toggle('hidden');
         })
         .catch(error => console.log(error));
     }
     else if(api === 'api2'){
+        _alterImage.classList.toggle('hidden');
         const objId = Math.ceil(Math.random() * 5000) + 6000;
         console.log("objId:",objId);
         fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objId}`)
@@ -41,24 +46,58 @@ function handleApiClick(event=null, random=null){
                     url = "https://images.metmuseum.org/CRDImages/ad/original/24516.jpg";
                 }
                 _alterImage.src = url;
+                _alterImage.classList.toggle('hidden');
             })
             .catch(error => {
                 console.log(error);
             })
-        console.log('api2');
     }
     else if(api === 'api3'){
-        console.log('api3');
+        //https://api.waifu.pics/sfw/waifu
+        _alterImage.classList.toggle('hidden');
+        fetch('https://api.waifu.pics/sfw/bonk')
+            .then(result => {
+                const data = result.json();
+                return data;
+            })
+            .then(data => {
+                const url = data.url;
+                _alterImage.src = url;
+                _alterImage.classList.toggle('hidden');
+            })
     }
     else if(api === 'api4'){
+        _alterImage.classList.toggle('hidden');
+        fetch('https://foodish-api.com/api/')
+            .then(result => {
+                const data = result.json();
+                return data;
+            })
+            .then(data => {
+                const url = data.image;
+                _alterImage.src = url;
+                _alterImage.classList.toggle('hidden');
+            })
         console.log('api4');
     }
     else if(api === 'api5'){
-        console.log('api5');
+        _alterImage.classList.toggle('hidden');
+        fetch('https://api.waifu.pics/sfw/waifu')
+            .then(result => {
+                const data = result.json();
+                return data;
+            })
+            .then(data => {
+                const url = data.url;
+                _alterImage.src = url;
+                _alterImage.classList.toggle('hidden');
+            })
+
     }
     else if(api === 'api6'){
         const rand = Math.ceil(Math.random() * 5);
         handleApiClick(null, `api${rand}`);
     }
+    _gif.classList.remove('hidden');
 }
 handleApiClick();
