8d665fad2df5d3fc9e9dd9a75b3c18df4b40526b
Amit Kubani <amitkubani@gmail.com>
2024-10-14 14:14:08 +0300

    commit

--- 2024-10/faq-accordion-main/main.js
+++ 2024-10/faq-accordion-main/main.js
@@ -2,13 +2,17 @@ const items = document.querySelectorAll('.item');
 
 function handleItemClick(event){
     let itemText;
-
     if(event.target.classList.contains('item')){
         itemText = event.target.querySelector('.itemText');
     }
     else{
         itemText = event.target.parentElement.querySelector('.itemText');
     }
+    const item = itemText.parentElement;
+    const buttons = item.querySelectorAll('img');
+    buttons.forEach(button => {
+        button.classList.toggle('hidden');
+    });
     itemText.classList.toggle('hidden');
 }
 
