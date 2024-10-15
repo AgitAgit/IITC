b1aa9dbeef0c85f89c774847b2cfac82b7f1f5a0
Amit Kubani <amitkubani@gmail.com>
2024-10-15 11:24:49 +0300

    commit

--- 2024-10/crud_and_cors/main.js
+++ 2024-10/crud_and_cors/main.js
@@ -1,5 +1,11 @@
 const _baseUrl = 'https://api-playground-ten.vercel.app';
 
+const _createPost = document.querySelector('#createPost');
+_createPost.addEventListener('submit', (event) => handleSubmitCreatePost(event));
+
+const _patchPost = document.querySelector('#patchPost');
+_patchPost.addEventListener('submit', event => handleSubmitPatchPost(event));
+
 //exercise 8
 async function getPosts() {
     const data = await axios.get(`${_baseUrl}/posts`);
@@ -26,12 +32,39 @@ async function getPostById(id){
     console.log(post.data);
 }
 
+//exercise 11
 async function createPost(title, content){
-    const response = axios.post(`${_baseUrl}/posts`,{
+    const response = await axios.post(`${_baseUrl}/posts`,{
         title,
         content
-    })
+    });
+    console.log(response.data);
 }
-// getPosts();
+function handleSubmitCreatePost(event){
+    event.preventDefault();
+    const {title, content} = event.target.elements;
+    createPost(title.value, content.value);
+    title.value = '';
+    content.value = '';
+}
+function toggleElement(id){
+    const element = document.querySelector(`#${id}`);
+    element.classList.toggle('hidden');
+}
+
+//exercise 12
+async function patchPost(title, content, postId){
+    const response = await axios.patch(`${_baseUrl}/posts/${postId}`,{
+        title,
+        content
+    });
+    console.log(response.data);
+}
+
+function handleSubmitPatchPost(title, content){
+
+}
+getPosts();
 // postsToList();
 // getPostById('670d1480b45f25be4c034e97');
+// createPost('3000 title','3000 content');
