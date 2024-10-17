fbbf907b58718064c08ff9bfbf286db92591b415
agit <box999box@gmail.com>
2024-10-17 14:05:38 +0300

    commit

--- 2024-10/sukot_project_tmdb/main.js
+++ 2024-10/sukot_project_tmdb/main.js
@@ -5,17 +5,18 @@ const _API_KEY_PROMISE = fetch('privateData.json')
 })
 const _baseUrl = 'https://api.themoviedb.org/3';
 const _baseImgUrl = 'https://image.tmdb.org';
-
+const _query1 = `${_baseUrl}/discover/movie?api_key=${_API_KEY}&primary_release_date.gte=${dateGte}&primary_release_date.lte=${dateLte}&sort_by=primary_release_date.asc`;
+const _query2 = `${_baseUrl}/discover/movie?api_key=${_API_KEY}&page=${page}`; 
 // _API_KEY.then(result => console.log(result));
 
-async function getPopulars(_API_KEY, page){
-    return await fetch(`${_baseUrl}/discover/movie?api_key=${_API_KEY}&sort_by=popularity.desc&page=${page}`)
-    .then(response => response.json())
-    .then(data => {
-        // console.log(data);
-        return data.results;
-    });
-}
+// async function getPopulars(_API_KEY, page){
+//     return await fetch(`${_baseUrl}/discover/movie?api_key=${_API_KEY}&sort_by=popularity.desc&page=${page}`)
+//     .then(response => response.json())
+//     .then(data => {
+//         // console.log(data);
+//         return data.results;
+//     });
+// }
 function logConfigurationData(){
     fetch(`${_baseUrl}/configuration?api_key=${_API_KEY}`)
     .then(response => response.json())
@@ -23,11 +24,11 @@ function logConfigurationData(){
         console.log(data);
     })
 }
-async function getPopulars(_API_KEY, page, dateGte, dateLte){
-    return await fetch(`${_baseUrl}/discover/movie?api_key=${_API_KEY}&page=${page}&`)
+async function getMovies(query){
+    return await fetch(query)
     .then(response => response.json())
     .then(data => {
-        // console.log(data);
+        console.log(data);
         return data.results;
     });
 }
@@ -53,10 +54,11 @@ async function main(){
     const earlierDate = new Date(currentDate);
     earlierDate.setDate(currentDate.getDate() - 7);
     const currentDateString = currentDate.toISOString().slice(0,10);
-    const page = 2;
-    //const data = await getPopulars(_API_KEY, page);
-    //console.log(data);
-    //displayMovies(data);
+    const earlierDateString = earlierDate.toISOString().slice(0,10);
+    const page = 1;
+    const data = await getMovies(_API_KEY, page, currentDateString, currentDateString);
+    console.log(data);
+    displayMovies(data);
     console.log(currentDate);
     console.log(earlierDate);
     // console.log(Date.now().toISOString());
