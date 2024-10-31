b7000bc1e5f072f32e00b19e25f34c96c878cf2c
Amit Kubani <amitkubani@gmail.com>
2024-10-31 08:00:21 +0200

    commit

---
+++ 2024-10/node_practice/practice2/models/jokeModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const jokeSchema = new mongoose.Schema({
+    id:Number,
+    setup:String,
+    punchline:String
+})
+
+const output = mongoose.model('Joke', jokeSchema);
+
+export default output;
