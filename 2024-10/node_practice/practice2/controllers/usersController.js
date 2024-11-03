a21026cc4ec78aac924ee042bb8ed5b1c10a4c72
agit <box999box@gmail.com>
2024-11-03 19:39:25 +0200

    commit

--- 2024-10/node_practice/practice2/controllers/usersController.js
+++ 2024-10/node_practice/practice2/controllers/usersController.js
@@ -1,5 +1,15 @@
 import User from '../models/userModel.js';
 
+export const getUserById = async function(req, res, next){
+    try{
+        const user = await User.findById(req.params.id);
+        res.json(user);
+        next();
+    } catch(error){
+        next(error);
+    }
+}
+
 export const getAllUsers = async function(req, res, next){
     try{
         const users = await User.find();
@@ -22,6 +32,42 @@ export async function addUser(req, res,  next){
         });
         const newUser = await user.save();
         res.status(201).json(newUser);
+        next();
+    } catch(error){
+        next(error);
+    }
+}
+
+export async function addUsers(req, res, next){
+    try{
+        const users = req.body.users;
+        const reply = await User.insertMany(users);
+        res.json({message:"successfuly added users", users});
+        next();
+    } catch(error){
+        next(error);
+    }
+}
+
+export async function patchUser(req, res, next){
+    try{
+        const newUser = req.body.user;
+        const user = await User.findById(req.body.user._id);
+        if(newUser.name) user.name = newUser.name;
+        if(newUser.password) user.password = newUser.password;
+        if(newUser.email) user.email = newUser.email;
+        const result = await user.save();
+        res.json({message:"patch successful", result});
+        next();
+    } catch(error){
+        next(error);
+    }
+}
+
+export const deleteUserById = async function(req, res, next){
+    try{
+        const reply = await User.findByIdAndDelete(req.params.id);
+        res.json({message:"delete successful", reply});
         next();
     } catch(error){
         next(error);
--- 2024-10/node_practice/practice2/routes/usersRoute.js
+++ 2024-10/node_practice/practice2/routes/usersRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllUsers, addUser } from '../controllers/usersController.js';
+import { getAllUsers, addUser, patchUser, addUsers, getUserById } from '../controllers/usersController.js';
 
 const router = express.Router();
 
@@ -8,46 +8,23 @@ router.get('/', getAllUsers, (req, res) => {
     res.send(res.body);
 });
 
-router.get('/random', (req, res) => {
-    const randomUser = users[Math.floor(Math.random() * users.length)]
-    res.send(randomUser)
-});
-
-router.get('/:id', (req, res) =>{
-    const user = users.find(user => user.id === parseInt(req.params.id));
-    if(user) res.send(user);
-    else res.send("user not found...");
+router.get('/:id', getUserById,(req, res) =>{
 });
 
 router.post('/single', addUser, (req, res) => {
 });
 
-router.post('/many', (req, res) => {
-    const newUsers = req.body;
-    users.push(...newUsers);
-    writeToFileSync('./db/users.json',JSON.stringify(users));  
-    res.send('users added!');
+router.post('/many', addUsers, (req, res) => {
 });
 
-router.patch('/:id', (req, res) => {//there is a problem "Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client"
-    const newUser = req.body;
-    const id = parseInt(req.params.id);
-    users.forEach(user => {
-        if(user.id === id){
-            users[users.indexOf(user)] = newUser;
-            writeToFileSync('./db/users.json', JSON.stringify(users));
-            res.send('patch successful!');
-        }
-    })
-    res.send('patch failed!');
+router.patch('/', patchUser,(req, res) => {
 })
 
 router.delete('/:id', (req,res) => {
-    const id = parseInt(req.params.id);
-    users = users.filter(user => user.id !== id);
+});
+
+router.use('/',(err, req, res, next) => {
     
-    writeToFileSync('./db/users.json', JSON.stringify(users));
-    res.send('delete successful!');
 });
 
 export default router;
