35f1e953c7828ea1453808010006f1b998a2daee
agit <box999box@gmail.com>
2024-11-20 13:37:37 +0200

    commit

--- 2024-11/react/pokedex/src/components/MiniPokemon/MiniPokemon.jsx
+++ 2024-11/react/pokedex/src/components/MiniPokemon/MiniPokemon.jsx
@@ -1,19 +1,23 @@
 import styles from './MiniPokemon.module.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import pokeballImg from './../../assets/pokeball_background.png';
 
 export default function MiniPokemon({ name, url }){
     const [pokemon, setPokemon] = useState(null);
+    const [cardClass, setCardClass] = useState(`${styles.card}`);
     //abilities[{ability:{ name, url},...},...],
     //sprites{front_default, back_default},
     //stats[{ base_stat, effort, stat:{name, url}},...]
     //types[{ slot, type:{name, url}},...]
     //height, weight, id, name, base_experience
 
+    //High quality sprites: sprites.other.dream_world.front_default
     async function fetchData() {
         try{
             const data = await axios.get(url);
             setPokemon(data.data);
+            setCardClass(previousCardClass => `${previousCardClass} ${styles[`${pokemon.types[0].type.name}`]}`)
             console.log(name, data.data);
         } catch (error){
             console.log(error);
@@ -24,19 +28,22 @@ export default function MiniPokemon({ name, url }){
         fetchData();
     },[])
 
-    return <div className={styles.card}>
-        { ( pokemon &&
-            <div className={styles[`${pokemon.types[0].type.name}`]}>
+    return ( 
+        <div className={cardClass}>
+            <img src={pokeballImg} className={styles.pokeballImg} />
+            { ( pokemon &&
+                <div className={styles[`${pokemon.types[0].type.name}`]}>
 
-                <div>
-                    <h2>{pokemon.name}</h2>
-                </div>
-                <div>
-                    image area
-                    <img src={pokemon.sprites.front_default} />
-                </div>
+                    <div>
+                        <h2>{pokemon.name}</h2>
+                    </div>
+                    <div>
+                        image area
+                        <img src={pokemon.sprites.other.dream_world.front_default} className={styles.sprite}/>
+                    </div>
 
-            </div>
-        )}
-    </div>
+                </div>
+            )}
+        </div>
+    )
 }
