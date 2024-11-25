a3f232c1fe6c16559e24c75ac6bae95e52e7069e
Amit Kubani <amitkubani@gmail.com>
2024-11-25 16:07:44 +0200

    commit

--- 2024-11/react/pokedex/src/components/Pokemon/Pokemon.jsx
+++ 2024-11/react/pokedex/src/components/Pokemon/Pokemon.jsx
@@ -1,16 +1,29 @@
+// import './Pokemon.module.css';
 import styles from './Pokemon.module.css';
 import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import backArrow from '../../assets/Images/backArrow.svg';
 import Utils from '../../utils/Utils';
 
 export default function Pokemon({ pokemon, pokeballImg, handleBackClick }){
+    const [currentTab, setCurrentTab] = useState();
+    const aboutButton = useRef();
     //abilities[{ability:{ name, url},...},...],
     //sprites{front_default, back_default},
     //stats[{ base_stat, effort, stat:{name, url}},...]
     //types[{ slot, type:{name, url}},...]
     //height, weight, id, name, base_experience
+
+    useEffect(() => {
+        handleTabClick(aboutButton.current);
+    },[])
+
+    function handleTabClick(element){
+        currentTab?.classList.remove(styles['selected-tab']);
+        element.classList.add(styles['selected-tab']);
+        setCurrentTab(element);
+    }
     
     return(
         <div className={ `${styles.card} ${styles[`${pokemon.types[0].type.name}`]}` }>
@@ -31,7 +44,10 @@ export default function Pokemon({ pokemon, pokeballImg, handleBackClick }){
                     
                     <div className={ styles['inner-data-div'] }>
                         <div className={styles['tabs-div']}>
-                            About, Base stats, Evolutions, moves
+                            {/* About, Base stats, Evolutions, moves */}
+                            <button className={`${styles.tab}`} onClick={(event) => handleTabClick(event.target)} ref={aboutButton}>About</button>
+                            <button className={styles.tab} onClick={(event) => handleTabClick(event.target)}>Base stats</button>
+                            <button className={styles.tab} onClick={(event) => handleTabClick(event.target)}>Moves</button>
                         </div>
                         <div>
                             stats section: hp, attack, defense, speed
