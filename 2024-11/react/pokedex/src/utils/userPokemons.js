629419dd3dedcbbcd31f5d29fcfd8f1bacb320a2
agit <box999box@gmail.com>
2024-11-27 20:07:30 +0200

    commit

--- 2024-11/react/pokedex/src/utils/userPokemons.js
+++ 2024-11/react/pokedex/src/utils/userPokemons.js
@@ -9,10 +9,23 @@ export default {
             localStorage.setItem('userPokemons', JSON.stringify([pokemon]));
         }
     },
+    validatePokemon: (pokemon) => {
+        if(pokemon.name === "" || pokemon.imageUrl === "" || !pokemon.types[0] || !pokemon.stats[5] 
+            || !pokemon.abilities[0] || !pokemon.baseExperience || !pokemon.weight || !pokemon.height) {
+                return false;
+        }
+        return true;
+    },
+    getLatestPokemonId: ()=>{
+        
+    },
     createUserPokemon: (name, imageUrl, types, stats, abilities, baseExperience, weight, height) => {
-        //1. validate fields
-        //2. assign unique id
-        //3. generate pokemon object
+        //1. generate pokemon object
+        const pokemon = { name, imageUrl, types, stats, abilities, baseExperience, weight, height }
+        //2. validate fields
+        if(!this.validatePokemon(pokemon)) return;
+        //3. assign unique id
+
         //4. add to user pokemons
     }
 }
