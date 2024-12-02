d65b0c92b2634d92cf39cebd35f056196335c517
agit <box999box@gmail.com>
2024-12-02 19:54:16 +0200

    commit

---
+++ 2024-11/react/pokedex/src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import { useNavigate } from 'react-router-dom';
+
+
+function ErrorPage(){
+    return <>
+        <h1>
+        404 Page Not Found
+        </h1>
+        <button></button>
+    </>
+}
