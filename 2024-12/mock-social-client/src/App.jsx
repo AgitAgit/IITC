32847c0f1fa24603e34fb33e127ef13d4b21b4ca
agit <box999box@gmail.com>
2024-12-04 15:10:45 +0200

    commit

---
+++ 2024-12/mock-social-client/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+//authentication(signup/login)
+//State management
+//Data fetching
+//Data editing
+
+import SignupForm from './components/SignupForm/SignupForm.jsx';
+
+function App() {
+
+  return (
+    <>
+      <SignupForm />
+    </>
+  )
+}
+
+export default App
