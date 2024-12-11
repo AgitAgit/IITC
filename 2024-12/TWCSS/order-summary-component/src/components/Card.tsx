86f45b18dba0fb627592796356b683141cff0e29
agit <box999box@gmail.com>
2024-12-11 14:21:52 +0200

    commit

---
+++ 2024-12/TWCSS/order-summary-component/src/components/Card.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Card
+() {
+  return (
+    <div className='h-96 w-40 bg-blue-500 border border-black'>Card
+        <div>
+            card in the card
+        </div>
+    </div>
+  )
+}
+
+export default 
+Card
