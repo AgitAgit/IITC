75bc3329eaf982c719c55594b401bff127acd06e
Amit Kubani <amitkubani@gmail.com>
2024-12-22 20:37:30 +0200

    commit

--- 2024-12/blog_post_crud/client/src/App.tsx
+++ 2024-12/blog_post_crud/client/src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [ posts, setPosts ] = useState(null);
+
+  useEffect(() => {
+    async function getData(){
+      const data = await axios.get('http://localhost:3000/api/posts');
+      console.log(data);
+      console
+    }
+  }, [])
 
   return (
     <>
+
     </>
   )
 }
