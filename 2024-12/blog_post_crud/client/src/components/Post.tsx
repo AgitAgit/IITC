e9779420096be15c5419dea36f2c9d7a8034f456
agit <box999box@gmail.com>
2024-12-24 14:58:01 +0200

    added mutate to edit a post

--- 2024-12/blog_post_crud/client/src/components/Post.tsx
+++ 2024-12/blog_post_crud/client/src/components/Post.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export type PostProps = {
     id:string,
@@ -8,12 +9,14 @@ export type PostProps = {
 }
 
 export default function Post( {id, title, content, deletePost}:PostProps) {
+  const navigate = useNavigate();
+
   return (
     <div className='border border-1 m-1'>
         <h2>{title}</h2>
         <p>{content}</p>
         <button>view</button>
-        <button>edit</button>
+        <button onClick={() => navigate('/edit', { state: { id, title, content }})}>edit</button>
         <button onClick={() => deletePost(id)}>delete</button>
     </div>
   )
--- 2024-12/blog_post_crud/client/src/pages/EditBlogPostPage.tsx
+++ 2024-12/blog_post_crud/client/src/pages/EditBlogPostPage.tsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import axios from 'axios';
+import { useRef } from 'react'
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function EditBlogPostPage() {
+  const location = useLocation();
+  const { id, title, content } = location.state;
+  const titleRef = useRef(title);
+  const contentRef = useRef(content);
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const { mutateAsync: editPost, isLoading, error } = useMutation({
+    mutationFn: async (id) => {
+      const result = await axios.put(`http://localhost:3000/api/posts/${id}`, {
+        title: titleRef.current.value,
+        content: contentRef.current.value
+      });
+      console.log(result.data);
+      return result.data;
+    },
+    onSettled: () => {
+      navigate('/');
+    }
+  })
+
+  async function handleEditClick() {
+    const result = await editPost(id);
+  }
+
   return (
-    <div>EditBlogPostPage</div>
+    <div>
+      EditBlogPostPage
+      <br></br>
+      <label>ID:{id}</label>
+      <br></br>
+      <label>title</label>
+      <input ref={titleRef} defaultValue={title}></input>
+      <br></br>
+      <label>content</label>
+      <input ref={contentRef} defaultValue={content}></input>
+      <button onClick={handleEditClick}>Edit</button>
+    </div>
   )
 }
