09c77e6feafba5634444cd09db6a79bb044aa9f7
agit <box999box@gmail.com>
2025-01-21 17:53:18 +0200

    commit

---
+++ 2025-01/python_basics/sandbox1/homework.py
@@ -0,0 +1,44 @@
+#e1
+"""
+username = input("enter your name")
+print("hello " + username)
+"""
+
+#e2
+# a = int(input("a = "))
+# b = int(input("b = "))
+# print("a + b = " + str(a) + " + " + str(b) + " = " + str(a + b))
+# print("a - b = " + str(a) + " - " + str(b) + " = " + str(a - b))
+# print("a * b = " + str(a) + " * " + str(b) + " = " + str(a * b))
+# print("a / b = " + str(a) + " / " + str(b) + " = " + str(a / b))
+
+
+#e3
+# a = int(input("a = "))
+# print("a ^ 2 = " + str(a) + " ^ 2 = " + str(a * a))
+
+# for i in range(3):
+#     a = int(input("a = "))
+#     if(a % 2 == 0):
+#         print(str(a) + " is even")
+#     else:
+#         print(str(a) + " is odd")
+
+# vowels = ['a','e','i','o','u']
+# character = input("enter a character:")
+# isVowel = False
+# for element in vowels:
+#     if(character == element):
+#         isVowel = True
+# if(isVowel):
+#     print(character + ' is a vowel')
+# else:
+#     print(character + ' is not a vowel')
+        
+# a = int(input("a = "))
+# b = 0
+# for i in range(a + 1):
+#     b += i
+# print("The sum of numbers from 0 to ", str(a), " is ", str(b))
+
+
