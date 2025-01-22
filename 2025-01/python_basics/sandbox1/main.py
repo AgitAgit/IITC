f80bc2e51a5f0bb219282dce2bd04e67c716500d
Amit Kubani <amitkubani@gmail.com>
2025-01-22 09:50:34 +0200

    commit

--- 2025-01/python_basics/sandbox1/main.py
+++ 2025-01/python_basics/sandbox1/main.py
@@ -1,1 +1,33 @@
-print("hello world")
+# print("hello world")
+
+# fruits = ["banana","kiwi"]
+
+# print(fruits)
+
+# fruits.append("apple")
+
+# fruits.append("apple")
+
+# print(fruits)
+
+# fruits.remove("apple")
+
+# print(fruits)
+
+# contact = {'name':'Alice', 'age':30}
+# contact['email'] = 'alice@example.com'
+# del contact['age']
+# print(contact)
+
+obj = {
+    'name':'duck3000',
+    'car':"Ha'agalash",
+    True:"tr",
+    False:"fl"
+}
+
+del obj[False]
+
+obj[("insufficient sleep","Arak")] = ["Reading","Offroad driving"]
+
+print(obj);
