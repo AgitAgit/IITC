e29b529b2022d50d6fa52e40e110d6d29c6ecbb1
agit <box999box@gmail.com>
2025-01-26 19:32:00 +0200

    commit

--- 2025-01/python_basics/sandbox2/kwargs.py
+++ 2025-01/python_basics/sandbox2/kwargs.py
@@ -1,3 +1,5 @@
+import numbers
+
 def kwargs1(**kwargs):
     for key, value in kwargs.items():
         print(f"{key}:{value}")
@@ -9,4 +11,22 @@ def kwargs_test(**bbb):
         print(f"key:{a}")
         print(f"value:{b}")
 
-kwargs_test(amy="3000", bronze="bomber")
+# kwargs_test(amy="3000", bronze="bomber")
+
+def kwargs2(**kwargs):
+    for key, value in kwargs.items():
+        if(key == "name"):
+            return value
+    return "Unknown"
+
+# print(kwargs2(properties="marak"))
+
+def kwargs3(**kwargs):
+    sum = 0
+    for key, value in kwargs.items():
+        if(isinstance(value, numbers.Real)):
+            sum += value
+    return sum
+
+# result = kwargs3(name="bbb", num1=1, num2=0.5, num3=-0.1)
+# print(result)
