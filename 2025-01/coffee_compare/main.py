049e869dc36d8dc6ab8ef2ae99ce8712266d04da
Amit Kubani <amitkubani@gmail.com>
2025-02-04 12:17:48 +0200

    commit

--- 2025-01/coffee_compare/main.py
+++ 2025-01/coffee_compare/main.py
@@ -1,6 +1,9 @@
 # general goal: 
 # compare the price of black coffee from at least 3 different vendors, and update it when wanted. 
 
+# connection goal:
+# insert to local file
+
 # next program: generate a tracking system for some basic commodities collection that will
 # help gauge how pricy is life. Track changes. Create a script that will run once a day
 # and record this information in sql or some other format for long term tracking.
@@ -10,24 +13,31 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import datetime
 
 import utils
 import shufersal_funcs
+import db_handle
 
 driver = webdriver.Chrome()
 driver.get("https://www.shufersal.co.il/online/he/%D7%A7%D7%98%D7%92%D7%95%D7%A8%D7%99%D7%95%D7%AA/%D7%A1%D7%95%D7%A4%D7%A8%D7%9E%D7%A8%D7%A7%D7%98/%D7%9E%D7%A9%D7%A7%D7%90%D7%95%D7%AA-%D7%90%D7%9C%D7%9B%D7%95%D7%94%D7%95%D7%9C-%D7%95%D7%99%D7%99%D7%9F/%D7%A7%D7%A4%D7%94-%D7%95%D7%AA%D7%94/%D7%A7%D7%A4%D7%94-%D7%A9%D7%97%D7%95%D7%A8/c/A130507")
 driver.maximize_window()
 elements = driver.find_elements(By.XPATH, "//li[@data-product-name]")
 
+db_handle.clear_db()
+
 for element in elements:
     new_element = element.find_element(By.XPATH,'.//div[@class="flex-line"]')
     print("\n\n-------------------------------------------")
     # print(reverse_string(element.text))
-    print(new_element.text)
+    # print(new_element.text)
+    product_name = shufersal_funcs.get_product_name(element)
     print(2*shufersal_funcs.get_product_price(element))
+    db_handle.add_record(product_name, "shufersal", shufersal_funcs.get_product_price(element), str(datetime.date.today()))
 
-time.sleep(1)
+# time.sleep(1)
 driver.quit()
+
 
 #//tagName[@AttributeName="Value"]
 #data-product-name="קפה טורקי בצלופן"
--- 2025-01/coffee_compare/shufersal_funcs.py
+++ 2025-01/coffee_compare/shufersal_funcs.py
@@ -6,9 +6,18 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import utils
 
-def getShufersalProductName(element):
+def get_product_name(element):
     return element.get_attribute("data-product-name")
 
+# def get_product_name(driver, locator):  # Pass the driver and locator
+#     try:
+#         element = WebDriverWait(driver, 10).until(  # Wait up to 10 seconds
+#             EC.presence_of_element_located(locator)  # Wait for element to be present
+#         )
+#         return element.get_attribute("data-product-name")
+#     except Exception as e:
+#         print(f"Error getting product name: {e}")
+#         return "unknown"
 
 def get_product_price(element):
     new_element = element.find_element(By.XPATH,'.//div[@class="flex-line"]')
